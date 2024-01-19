853a540ba65a65ddb3d575f75d2859e001123c0f
danishebadulla326 <danish_ebadulla@ucsb.edu>
2024-01-19 07:33:43 +0000

    added worksheets 1 and 2

---
+++ worksheets/worksheet2/tests/q8_4.py
@@ -0,0 +1,3 @@
+OK_FORMAT = True
+
+test = {'name': 'q8_4', 'points': None, 'suites': [{'cases': [{'code': '>>> gradescope == 694423\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
